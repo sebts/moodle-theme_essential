ccc9a43a608fee5d696faacb94ff2b9d904605d7
Bryan Underwood <bunderwood@sebts.edu>
2015-09-08 08:30:04 -0400

    remove js alert from dev

--- javascript/sso.js
+++ javascript/sso.js
@@ -3,7 +3,6 @@ var loginform = document.getElementById('login');
 $(loginform).submit(function(){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-	alert("now essential");
 	$('<iframe />', {
                 width: '0',
                 height: '0',
